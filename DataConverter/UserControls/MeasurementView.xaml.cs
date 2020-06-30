cdd39269349e54905cca2639632e314b0b2c6769
derToaster <ashkanmoradi1412@yahoo.de>
2020-06-30 06:58:47 -0700

    init

---
+++ DataConverter/UserControls/MeasurementView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataConverter.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MeasurementView.xaml
+    /// </summary>
+    public partial class MeasurementView : UserControl
+    {
+        public MeasurementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
